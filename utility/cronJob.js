57b28e9c6a7f489d4854f11f5252a14021ebcff8
jordy756 <jordycastro1756@gmail.com>
2025-02-24 13:18:06 -0600

    Add error handling and improve post content in cron job function

--- utility/cronJob.js
+++ utility/cronJob.js
@@ -1,17 +1,19 @@
 export const insertPostEveryMinute = async () => {
-    console.log("Insertando post cada minuto");
-    const randomPageResponse = await fetch("http://localhost:3000/pages/getRamdomPage");
-    const randomPageData = await randomPageResponse.json();
+    try {
+        console.log("Insertando post cada minuto");
+        const randomPageResponse = await fetch("http://localhost:3000/pages/getRamdomPage");
+        const randomPageData = await randomPageResponse.json();
 
-    const response = await fetch(`http://localhost:3000/pages/insertPostInPage/${randomPageData._id}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            content: "Post de prueba",
-        }),
-    });
+        const response = await fetch(`http://localhost:3000/pages/insertPostInPage/${randomPageData._id}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                content: `Post que se inserta cada minuto ${new Date().toISOString()}`,
+            }),
+        });
 
-    console.log(await response.json());
+        console.log(await response.json());
+    } catch (error) {
+        console.error(error);
+    }
 };
