ce28df848113b566bc8e32e07401d9a8a8e5042c
CrissCordoba <criscordoba005@gmail.com>
2025-02-28 10:37:34 -0600

    Add comments to text controller and route for better code clarity

--- controllers/text.controller.js
+++ controllers/text.controller.js
@@ -1,23 +1,23 @@
-const Text = require("../models/Text");
+const Text = require("../models/Text"); // Importa el modelo Text
 
-const insertText = async (req, res) => {
+const insertText = async (req, res) => { //Metodo para insertar un texto
     try {
-        const { text } = req.body;
-        const newText = new Text({ text });
+        const { text } = req.body; //Extrae el texto del cuerpo de la solicitud
+        const newText = new Text({ text }); //Crea un nuevo objeto Text
 
-        res.status(201).json(await newText.save());
+        res.status(201).json(await newText.save()); //Guarda el objeto en la base de datos y devuelve el objeto guardado
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
-const getRandomText = async (req, res) => {
+const getRandomText = async (req, res) => { //Metodo para obtener un texto aleatorio
     try {
-        const [randomText] = await Text.aggregate([{ $sample: { size: 1 } }]);
-        res.status(200).json(randomText);
+        const [randomText] = await Text.aggregate([{ $sample: { size: 1 } }]); //Obtiene un texto aleatorio de la base de datos
+        res.status(200).json(randomText); //Devuelve el texto aleatorio
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 };
 
-module.exports = { insertText, getRandomText };
+module.exports = { insertText, getRandomText }; //Exporta los metodos insertText y getRandomText
--- routes/text.route.js
+++ routes/text.route.js
@@ -1,13 +1,13 @@
-const { Router } = require("express");
-const { insertText, getRandomText } = require("../controllers/text.controller");
+const { Router } = require("express"); // Importa el metodo Router de express
+const { insertText, getRandomText } = require("../controllers/text.controller"); // Importa los metodos insertText y getRandomText del controlador text.controller
 
-const createTextRouter = () => {
-    const router = Router();
+const createTextRouter = () => { //Crea un router para los metodos de text
+    const router = Router(); //Crea un router
 
-    router.post("/insertText", insertText);
-    router.get("/getRandomText", getRandomText);
+    router.post("/insertText", insertText); //Crea una ruta para insertar un texto
+    router.get("/getRandomText", getRandomText); //Crea una ruta para obtener un texto aleatorio
 
-    return router;
+    return router; //Devuelve el router
 };
 
-module.exports = { createTextRouter };
+module.exports = { createTextRouter }; //Exporta el metodo createTextRouter
