5f3086f14912924a9bf6ebbf4d99d761c639d3f6
CrissCordoba <criscordoba005@gmail.com>
2025-02-28 18:38:43 -0600

    Enhance code clarity by adding comments in config, session, and auth middleware files; improve error messages and logging

--- db/config.js
+++ db/config.js
@@ -1,12 +1,12 @@
-const { connect } = require("mongoose");
-const { CONNECTION_STRING_DB } = require("../utility/environment");
+const { connect } = require("mongoose"); //se exporta el connect de mongoose 
+const { CONNECTION_STRING_DB } = require("../utility/environment"); //Se obtiene el string de conexion del enviroment 
 
-const getConnection = async () => {
+const getConnection = async () => { //Se define una funcion para conectarse a la base de datos
     try {
         await connect(CONNECTION_STRING_DB);
-        console.log("Base de datos conectada correctamente");
+        console.log("Database connected");
     } catch (error) {
-        console.error("Error al conectar a la base de datos", error);
+        console.error("Error connecting the database", error);
     }
 };
 
--- middlewares/SessionAuth.js
+++ middlewares/SessionAuth.js
@@ -1,16 +1,28 @@
+
 const jwt = require("jsonwebtoken");
+// Se importa la clave secreta 'SECRET_KEY' desde el enviroment 
 const { SECRET_KEY } = require("../utility/environment");
 
 const sessionMiddleware = (req, res, next) => {
+    // Obtiene el token de acceso (JWT) desde las cookies de la solicitud HTTP
     const token = req.cookies.access_token;
+
+    // Inicializa la sesion del usuario como nula
     req.session = { user: null };
 
     try {
+        // Si hay un token, intenta verificarlo y decodificarlo usando la clave secreta
         const userData = jwt.verify(token, SECRET_KEY);
-        req.session.user = userData;
-    } catch {}
 
+        // Si el token es valido, asigna la informacion del usuario decodificada al objeto de la sesion
+        req.session.user = userData;
+    } catch {
+        // Si el token es invalido o ha expirado, no hace nada y la sesion permanecera como nula.
+    }
+
+    // Llama al siguiente middleware o controlador en la pila de middleware de Express
     next();
 };
 
+// Exporta el middleware para que pueda ser usado en otras partes de la aplicacion 
 module.exports = { sessionMiddleware };
